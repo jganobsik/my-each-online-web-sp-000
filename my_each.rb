b0ae771ca2ae571564f088fb5f4f3a3e4f3e7caf
Joe Ganobsik <jganobsik@gmail.com>
2018-12-19 02:48:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,12 @@
 
-
-def my_each(collection) # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
   i = 0 
-  while i < collection.length
-  item = collection[i]
+  while i < array.length
+  item = array[i]
   yield(item)
   i += 1 
 end
 end
 
-my_each { |item| puts item}
+my_each |item| puts item}
